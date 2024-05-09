f4eabdf7aa909b398e46bf2e0e0ea51ad0da404d
Zoe Ho <wpdevtw@gmail.com>
2024-05-09 22:10:56 +0800

    8800項目進行中

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// 母音
-const vowels = ["images/280/vowels/45.png", "images/280/vowels/46.png", "images/280/vowels/47.png", "images/280/vowels/48.png", "images/280/vowels/49.png", "images/280/vowels/50.png", "images/280/vowels/62.png", "images/280/vowels/63.png", "images/280/vowels/64.png", "images/280/vowels/4647.png"];
+// 280母音
+const vowels280 = ["images/280/vowels/45.png", "images/280/vowels/46.png", "images/280/vowels/47.png", "images/280/vowels/48.png", "images/280/vowels/49.png", "images/280/vowels/50.png", "images/280/vowels/62.png", "images/280/vowels/63.png", "images/280/vowels/64.png", "images/280/vowels/4647.png"];
 // 豬豬家族
 const consonantsPartOne = ["images/280/consonants/dr-3260.png", "images/280/consonants/tr-3360.png", "images/280/consonants/br-13560.png", "images/280/consonants/pr-23560.png"];
 // 樂樂家族
@@ -7,7 +7,14 @@ const consonantsPartTwo = ["images/280/consonants/bl-119.png", "images/280/conso
 // 思思家族
 const consonantsPartThree = ["images/280/consonants/sm-383.png", "images/280/consonants/sn-3812.png", "images/280/consonants/sp-381.png", "images/280/consonants/st-3814.png", "images/280/consonants/sk-3821.png", "images/280/consonants/str-383260.png"];
 
+// 8800母音
+const vowels8800 = ["images/8800/vowels/44.png", "images/8800/vowels/45.png", "images/8800/vowels/46.png", "images/8800/vowels/47.png", "images/8800/vowels/48.png", "images/8800/vowels/49.png", "images/8800/vowels/50.png", "images/8800/vowels/51.png", "images/8800/vowels/62.png", "images/8800/vowels/63.png", "images/8800/vowels/64.png", "images/8800/vowels/66.png", "images/8800/vowels/4347.png", "images/8800/vowels/4359.png", "images/8800/vowels/4547.png", "images/8800/vowels/4647.png", "images/8800/vowels/5960.png"];
+// 8800頭子音
+const beginningConsonants8800 = ["images/8800/consonants/beginning/01.png", "images/8800/consonants/beginning/02.png", "images/8800/consonants/beginning/03.png", "images/8800/consonants/beginning/04.png", "images/8800/consonants/beginning/12.png", "images/8800/consonants/beginning/14.png", "images/8800/consonants/beginning/15.png", "images/8800/consonants/beginning/19.png", "images/8800/consonants/beginning/21.png", "images/8800/consonants/beginning/22.png", "images/8800/consonants/beginning/23.png", "images/8800/consonants/beginning/38.png", "images/8800/consonants/beginning/40.png", "images/8800/consonants/beginning/3260.png", "images/8800/consonants/beginning/3360.png", "images/8800/consonants/beginning/3560.png", "images/8800/consonants/beginning/3801.png", "images/8800/consonants/beginning/3814.png", "images/8800/consonants/beginning/3821.png", "images/8800/consonants/beginning/383260.png"];
+
 let consonants = [];
+let vowels = [];
+
 let currentIndex = 0;
 let shuffledCards = [];
 let consonantImg = document.getElementById("consonant");
@@ -15,6 +22,7 @@ let vowelImg = document.getElementById("vowel");
 let startButton = document.getElementById('start');
 let counter = document.getElementById('counter');
 let suah = document.getElementById('suah'); // ‧ㄙㄨㄚ
+let descText = document.getElementById('descText'); // 一般說明文字
 let keySpaceHint = document.getElementById('keySpaceHint');
 let audioPlayer = document.getElementById('audioPlayer');
 
@@ -60,6 +68,7 @@ function initializeShuffle() {
 
     shuffledCards = [];
     consonants = [];
+    vowels = vowels280;
 
     // 豬豬家族
     if (document.getElementById('partOne')) {
@@ -76,6 +85,11 @@ function initializeShuffle() {
     // 280攻頂
     if (document.getElementById('all')) {
         consonants = consonants.concat(consonantsPartOne, consonantsPartTwo, consonantsPartThree);
+    }
+    // 8800
+    if (document.getElementById('sounds8800')) {
+        consonants = beginningConsonants8800;
+        vowels = vowels8800;
     }
 
     shuffleArray(consonants);
@@ -97,8 +111,10 @@ function initializeShuffle() {
 
 }
 
+initializeShuffle();
+
 // 豬豬家族
-function addIdtoTabPaneOne() {
+document.getElementById('pills-part-one').onclick = () => {
     let tabPane = document.querySelector('.tab-pane');
     if (tabPane) {
         tabPane.id = 'partOne';
@@ -147,19 +163,33 @@ function addIdtoTabPaneAll() {
 
 // 隨機音素組合
 function showRandomCard() {
+    if (document.getElementById('sounds8800')) {
+        descText.style.display = 'inline-block';
+    }
+
     startButton.textContent = '下一個';
 
-    // 小螢幕不顯示空白鍵提示
-    var currentWidth = window.innerWidth;
-    var maxWidth = 768;
-    if (currentWidth >= maxWidth) {
+
+    let currentWidth = window.innerWidth;
+    let mediumDevices = 768;
+    let smallDevices = 576;
+    // 螢幕寬度大於768時才顯示空白鍵提示
+    if (currentWidth >= mediumDevices) {
         keySpaceHint.style.display = 'block';
     }
+    // 螢幕小於576時將「+20個尾子音」換行
+    if (document.getElementById('sounds8800') && currentWidth <= smallDevices) {
+        descText.style.display = 'block';
+    }
+
 
     if (currentIndex < shuffledCards.length) {
         const card = shuffledCards[currentIndex];
         consonantImg.src = card.consonant;
         vowelImg.src = card.vowel;
+
+        audioPlayer.pause();
+        audioPlayer.currentTime = 0;
 
         // 檢查組合並顯示對應音檔
         const bricks = [
@@ -863,21 +893,192 @@ function showRandomCard() {
                 vowelBrick: "images/280/vowels/4647.png",
                 audioSrc: "audios/280/c383260v4647.mp3",
             },
+            // 8800 ㄅ
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/49.png",
+                audioSrc: "audios/8800/c01v49_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/45.png",
+                audioSrc: "audios/8800/c01v45_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/62.png",
+                audioSrc: "audios/8800/c01v62_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/46.png",
+                audioSrc: "audios/8800/c01v46_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/44.png",
+                audioSrc: "audios/8800/c01v44_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/48.png",
+                audioSrc: "audios/8800/c01v48_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/66.png",
+                audioSrc: "audios/8800/c01v66_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/51.png",
+                audioSrc: "audios/8800/c01v51_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/64.png",
+                audioSrc: "audios/8800/c01v64_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/5960.png",
+                audioSrc: "audios/8800/c01v5960_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/4647.png",
+                audioSrc: "audios/8800/c01v4647_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/47.png",
+                audioSrc: "audios/8800/c01v47_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/4347.png",
+                audioSrc: "audios/8800/c01v4347_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/63.png",
+                audioSrc: "audios/8800/c01v63_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/50.png",
+                audioSrc: "audios/8800/c01v50_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/4359.png",
+                audioSrc: "audios/8800/c01v4359_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/01.png",
+                vowelBrick: "images/8800/vowels/4547.png",
+                audioSrc: "audios/8800/c01v4547_8800.mp3",
+            },
+            // 8800 ㄆ
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/49.png",
+                audioSrc: "audios/8800/c02v49_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/45.png",
+                audioSrc: "audios/8800/c02v45_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/62.png",
+                audioSrc: "audios/8800/c02v62_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/46.png",
+                audioSrc: "audios/8800/c02v46_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/44.png",
+                audioSrc: "audios/8800/c02v44_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/48.png",
+                audioSrc: "audios/8800/c02v48_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/66.png",
+                audioSrc: "audios/8800/c02v66_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/51.png",
+                audioSrc: "audios/8800/c02v51_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/64.png",
+                audioSrc: "audios/8800/c02v64_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/5960.png",
+                audioSrc: "audios/8800/c02v5960_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/4647.png",
+                audioSrc: "audios/8800/c02v4647_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/47.png",
+                audioSrc: "audios/8800/c02v47_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/4347.png",
+                audioSrc: "audios/8800/c02v4347_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/63.png",
+                audioSrc: "audios/8800/c02v63_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/50.png",
+                audioSrc: "audios/8800/c02v50_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/4359.png",
+                audioSrc: "audios/8800/c02v4359_8800.mp3",
+            },
+            {
+                consonantBrick: "images/8800/consonants/beginning/02.png",
+                vowelBrick: "images/8800/vowels/4547.png",
+                audioSrc: "audios/8800/c02v4547_8800.mp3",
+            },
         ];
 
         for (const brick of bricks) {
             if (card.consonant === brick.consonantBrick && card.vowel === brick.vowelBrick) {
-                audioPlayer.style.display = 'block';
 
                 // 確認音檔存在
                 if (brick.audioSrc) {
                     audioPlayer.src = brick.audioSrc;
-                } else {
-                    audioPlayer.style.display = 'none';
+                    audioPlayer.style.display = 'block';
                 }
-
                 break;
             }
+
+            audioPlayer.style.display = 'none';
         }
 
         currentIndex++;
@@ -904,11 +1105,11 @@ function showRandomCard() {
 }
 
 // 當Modal關閉時停止影片播放
-var modals = document.querySelectorAll('.modal');
+let modals = document.querySelectorAll('.modal');
 modals.forEach(function (modal) {
     modal.addEventListener('hidden.bs.modal', function () {
-        var iframe = modal.querySelector('#youtubeIframe');
-        var iframeSrc = iframe.src;
+        let iframe = modal.querySelector('#youtubeIframe');
+        let iframeSrc = iframe.src;
         iframe.src = iframeSrc; // 重新設置iframe的src，以停止影片播放
     });
 });
